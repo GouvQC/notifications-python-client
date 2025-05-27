4eae8f53f721290cc2b95608ee88485bc106bc82
Kaouther.Hedhly-ext@mcn.gouv.qc.ca <Kaouther.Hedhly-ext@mcn.gouv.qc.ca>
2025-05-27 08:30:21 -0400

    corriger les vulnerabilites setuptools

---
+++ install_python_versions.sh
@@ -0,0 +1,8 @@
+RUN export PATH="/root/.pyenv/bin:$PATH" && \
+    export PYENV_ROOT="/root/.pyenv" && \
+    eval "$(pyenv init --path)" && \
+    eval "$(pyenv init -)" && \
+    for v in $(cat tox-python-versions); do \
+      pyenv install -s $v && \
+      $PYENV_ROOT/versions/$v/bin/pip install --upgrade setuptools==78.1.1; \
+    done
